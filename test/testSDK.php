20a59b747ed885112a8cb62e8011c9124f8a53a5
Jorge Orpinel <jorge@orpinel.com>
2017-01-08 13:41:06 -0500

    Updated testSDK.php to include service and schedule creation (incomplete)

--- test/testSDK.php
+++ test/testSDK.php
@@ -7,6 +7,8 @@
 require_once "../vendor/autoload.php";
 
 use GonebusyLib\GonebusyClient;
+use GonebusyLib\Models\CreateServiceBody;
+use GonebusyLib\Models\CreateScheduleBody;
 use GonebusyLib\Models\CreateBookingBody;
 
 // Authentication and initialization
@@ -14,17 +16,41 @@ $authorization = "Token 2ae673263145c48185f945880f185b2a";
 $client = new GonebusyClient($authorization);
 
 
+// Pre-requisites (service and schedule)
+
+// Get singleton Service instance
+$services = $client->getServices();
+
+$collect['authorization'] = $authorization;
+$createServiceBody = new CreateServiceBody();
+// TODO: Populate service body
+$collect['createServiceBody'] = $createServiceBody;
+
+$service = $services->createService($collect);
+var_dump( $service );
+
+// Get singleton Schedule instance
+$services = $client->getSchedules();
+
+$collect['authorization'] = $authorization;
+
+$createScheduleBody = new CreateScheduleBody();
+// TODO: Populate schedule body
+$collect['createScheduleBody'] = $createScheduleBody;
+
+$result = $schedules->createSchedule($collect);
+
+
 // Bookings Test
 
-// Get singleton instance
+// Get singleton Booking instance
 $bookings = $client->getBookings();
-
 
 $collect['authorization'] = $authorization;
 $createBookingBody = new CreateBookingBody();
+// TODO: Populate booking body
 $collect['createBookingBody'] = $createBookingBody;
 
 // Create a Booking with params
-$result = $bookings->createBooking($collect);
-
+$booking = $bookings->createBooking($collect);
 var_dump( $result );
