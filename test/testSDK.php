9eeb4ff54bc32157049c6761b6eeb7dac815e576
Jorge Orpinel <jorge@orpinel.com>
2017-01-08 13:26:57 -0500

    Updates test to use namespace

--- test/testSDK.php
+++ test/testSDK.php
@@ -6,9 +6,12 @@
 // include composer's autoloader to enable auto loading of classes
 require_once "../vendor/autoload.php";
 
+use GonebusyLib\GonebusyClient;
+use GonebusyLib\Models\CreateBookingBody;
+
 // Authentication and initialization
 $authorization = "Token 2ae673263145c48185f945880f185b2a";
-$client = new GonebusyLib\GonebusyClient($authorization);
+$client = new GonebusyClient($authorization);
 
 
 // Bookings Test
@@ -18,7 +21,7 @@ $bookings = $client->getBookings();
 
 
 $collect['authorization'] = $authorization;
-$createBookingBody = new GonebusyLib\Models\CreateBookingBody();
+$createBookingBody = new CreateBookingBody();
 $collect['createBookingBody'] = $createBookingBody;
 
 // Create a Booking with params
